e13479dab0f634e71a6ba6ec16c92eef9f9f0faa
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-04-08 17:49:06 +0100

    Final changes

--- src/com/qa/main/Runner.java
+++ src/com/qa/main/Runner.java
@@ -6,6 +6,9 @@ public class Runner
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
+		Factorials factor = new Factorials();
+		factor.getFactorialNumber(4);
+		
 
 	}
 
